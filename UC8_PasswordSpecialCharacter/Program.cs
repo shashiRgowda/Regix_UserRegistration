e31f4047aaefd1cfed8286100ea161688ca14256
shashiRgowda <shashikumartr64@gmail.com>
2021-05-19 14:10:48 +0530

    Added:UC8_PasswordSpecialCharacter

---
+++ UC8_PasswordSpecialCharacter/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace UserRegistration
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome To User Registration !");
+
+            string first_name = null;
+            string last_name = null;
+            string email = null;
+            string phone_number = null;
+            string password = null;
+
+            UserRegistration obj_userregistaration = new UserRegistration();
+
+
+            while (first_name is null)
+                first_name = obj_userregistaration.FristName();
+
+            while (last_name is null)
+                last_name = obj_userregistaration.LastName();
+
+            while (email is null)
+                email = obj_userregistaration.Email();
+
+            while (phone_number is null)
+                phone_number = obj_userregistaration.PhoneNumber();
+
+            while (password is null)
+                password = obj_userregistaration.Password();
+
+            Console.WriteLine("First name  : " + first_name);
+            Console.WriteLine("Last name  : " + last_name);
+            Console.WriteLine("Email  : " + email);
+            Console.WriteLine("Phone Number  : " + phone_number);
+            Console.WriteLine("Password  : " + password);
+        }
+    }
+}
