860afe6ad9c244b4bdac94c78243b497716fa004
shashiRgowda <shashikumartr64@gmail.com>
2021-05-19 13:21:22 +0530

    Added:UC2_LastName

---
+++ UC2_LastName/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace UserRegistration
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome To User Registration !");
+
+            string first_name = null;
+            string last_name = null;
+
+            UserRegistration obj_userregistaration = new UserRegistration();
+
+
+            while (first_name is null)
+                first_name = obj_userregistaration.FristName();
+
+            while (last_name is null)
+                last_name = obj_userregistaration.LastName();
+
+
+            Console.WriteLine("First name  : " + first_name);
+            Console.WriteLine("Last name  : " + last_name);
+        }
+    }
+}
---
+++ UC2_LastName/UserRegistration.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace UserRegistration
+{
+    class UserRegistration
+    {
+        public string FristName()
+        {
+
+            string first_name;
+            Console.WriteLine("Enter first name \n  ::first name start with cap and has minimum 3 charecters");
+            first_name = Console.ReadLine();
+
+            string exp = "^[A-Z]{1,}..";
+
+            Regex re_firstName = new Regex(exp);
+
+            if (re_firstName.IsMatch(first_name))
+            {
+                return first_name;
+            }
+            else
+            {
+                Console.WriteLine("in valid input ");
+                return null;
+            }
+        }
+        public string LastName()
+        {
+
+            string last_name;
+            Console.WriteLine("Enter Last name \n  :: last name start with cap and has minimum 3 charecters");
+            last_name = Console.ReadLine();
+
+            string exp = "^[A-Z]{1,}..";
+
+            Regex re_firstName = new Regex(exp);
+
+            if (re_firstName.IsMatch(last_name))
+            {
+                return last_name;
+            }
+            else
+            {
+                Console.WriteLine("in valid input ");
+                return null;
+            }
+        }
+    }
+}
