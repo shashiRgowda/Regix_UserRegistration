e52e6b697237aa7d0d4675578c5f5b2579bc2764
shashiRgowda <shashikumartr64@gmail.com>
2021-05-19 13:10:29 +0530

    Added;UC1_IsValidFirstName

---
+++ UC1_IsValidFirstName/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace UserRegistration
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string first_name;
+            Console.WriteLine("Welcome To User Registration !");
+
+            var obj_userregistaration = new UserRegistration();
+
+
+            first_name = obj_userregistaration.FristName();
+            if (first_name is null)
+                first_name = obj_userregistaration.FristName();
+
+
+            Console.WriteLine("first name  : " + first_name);
+        }
+    }
+}
---
+++ UC1_IsValidFirstName/Regex.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace UserRegistration
+{
+    internal class Regex
+    {
+        private string exp;
+
+        public Regex(string exp)
+        {
+            this.exp = exp;
+        }
+
+        internal bool IsMatch(string first_name)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
